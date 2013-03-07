6208b0ca22506baf0558cfefdf0ef1f95896f514
david karapetyan <dkarapetyan@gmail.com>
2013-03-06 21:58:20 -0800

    heartbeat re-establishment after broken pipe

--- client.rb
+++ client.rb
@@ -8,7 +8,7 @@ Thread.abort_on_exception = true
 
 module ClientRegistrationHeartbeatStateMachine
   def self.start
-    register; establish_heartbeat; monitor_heartbeat; accept_rpc_requests
+    register; establish_heartbeat; accept_rpc_requests
   end
   
   # keep trying until we successfully register
@@ -23,8 +23,7 @@ module ClientRegistrationHeartbeatStateMachine
     rescue Errno::ECONNREFUSED
       wait_period = 5
       puts "Registration connection refused. Retrying in #{wait_period} seconds."
-      sleep wait_period
-      retry
+      sleep wait_period; retry
     end
   end
   
@@ -33,23 +32,21 @@ module ClientRegistrationHeartbeatStateMachine
     @heartbeat_thread = Thread.new do
       loop do
         puts "Heartbeat."
-        @conn.puts "OK"; @conn.flush
-        sleep 5
+        begin
+          @conn.puts "OK"; @conn.flush
+          sleep 5
+        rescue Errno::EPIPE
+          break
+        end
       end
+      restart_heartbeat
     end
   end
   
-  # make sure hearbeat connection is working and take action to
-  # correct the error if we can't send the heartbeat
-  def self.monitor_heartbeat
-    @heartbeat_monitor_thread = Thread.new do
-      loop do
-        puts "Monitoring heartbeat thread. No mitigating action yet."
-        sleep 120
-      end
-    end
+  def restart_hearbeat
+    register; establish_hearbeat
   end
-
+  
   def self.accept_rpc_requests
     puts "Accepting rpc requests."
   end
