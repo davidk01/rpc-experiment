3d08b6eae6c6dc07a6bf3c17931dcd703719b9ae
david karapetyan <dkarapetyan@gmail.com>
2013-03-10 12:56:11 -0700

    manually managing partial reads is silly. a dsl is the right approach here.

---
+++ lib/partialreaderdsl.rb
@@ -0,0 +1,51 @@
+class Instruction; end
+
+class Consumer < Instruction
+  
+  def initialize(count)
+    @count, @buffer = count, ""
+  end
+  
+  def call(connection)
+    return_value = nil
+    if @buffer.length < @count
+      @buffer << connection.readpartial(@count - @buffer.length)
+    else
+      return_value = [@buffer, :done]
+    end
+    return_value
+  end
+  
+end
+
+class PartialReaderDSL
+  
+  def self.protocol(&blk)
+    current_instance = new
+    blk.call(current_instance)
+    current_instance
+  end
+  
+  def initialize
+    @buffer = ""
+    @instruction_sequence = []
+    @current_instruction_pointer = 0
+    @return_stack = []
+  end
+  
+  def consume(count)
+    @instruction_sequence << Consumer.new(count)
+  end
+  
+  def buffer_transform(&blk)
+    @return_stack << blk.call(self)
+  end
+  
+  def splice(*instructions)
+    current_instruction = @insturction_sequence[@current_instruction_pointer]
+    instructions.unshift(current_instruction)
+    @instruction_sequence[@current_instruction_pointer] = instructions
+    @instruction_sequence.flatten!
+  end
+    
+end
