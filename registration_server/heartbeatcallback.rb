ad5df9af8d560e0fb96ff7bf42ef39798a9c72b5
david karapetyan <dkarapetyan@gmail.com>
2013-03-10 17:32:59 -0700

    adsf

--- registration_server/heartbeatcallback.rb
+++ registration_server/heartbeatcallback.rb
@@ -1,17 +1,28 @@
 class HeartbeatCallback
   
   def initialize(beat, wipe)
-    @beat, @wipe = beat, wipe
+    @wipe = wipe
+    @machine = PartialReaderDSL::PartialReaderMachine.protocol do |m|
+      m.consume(2)
+      m.buffer_transform do |context|
+        if context.buffer == "OK"
+          $logger.debug "Heartbeat OK. Reseting machine."
+          beat.call; m.jump(-1)
+        else
+          $logger.error "Did not recognize heartbeat message: #{context.buffer}."
+          wipe.call
+        end
+      end
+    end
   end
   
-  # TODO: Add a timeout so that heartbeat loop isn't blocked forever
   def call(monitor)
     $logger.debug "Reading heartbeat data."
-    heartbeat = monitor.io.readpartial(2) rescue nil
-    if heartbeat == "OK"
-      $logger.debug "#{monitor.io.remote_address} is OK."; @beat.call
-    else
-      $logger.error "Message from #{monitor.io.remote_address}: #{heartbeat}."; @wipe.call
+    begin
+      @machine.call(monitor.io)
+    rescue EOFError
+      $logger.error "EOFError from #{monitor.io.remote_address}."
+      @wipe.call
     end
   end
   
