eb85d649815c308263be0c937878716dc999b227
david karapetyan <david@david-Dell-System-XPS-L322X.(none)>
2013-03-16 01:24:33 -0700

    simple plugin

---
+++ plugins/discovery.rb
@@ -0,0 +1,25 @@
+class Discovery
+  
+  # describe the plugin
+  def self.descriptive_name
+    "host.discovery"
+  end
+  def self.description
+    <<-EOF
+      Provides host facts and responds to ping requests so
+      that we can verify that an agent is indeed running.
+      This information can also be exposed via the hearbeat
+      mechanism but this plugin serves as a demonstration of
+      how plugins are structured.
+    EOF
+  end
+
+  # register as a plugin
+  include Plugins
+
+  # define some actions
+  def_action :name => "ping", :desc => "The agent responds with pong.", 
+   :args => {} do |opts = {}| 
+    {"response" => "pong"} 
+  end
+end
