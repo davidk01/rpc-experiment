12c827b64dc981f65ee1bca995d68af761cd2f2c
david karapetyan <david@david-Dell-System-XPS-L322X.(none)>
2013-04-21 13:33:51 -0700

    no reason to include the client subdirectory

--- config/warble.rb
+++ config/warble.rb
@@ -17,7 +17,7 @@ Warbler::Config.new do |config|
   config.includes = FileList["bin/*"]
 
   # Additional files/directories to exclude
-  # config.excludes = FileList["lib/tasks/*"]
+  config.excludes = FileList["lib/client"]
 
   # Additional Java .jar files to include.  Note that if .jar files are placed
   # in lib (and not otherwise excluded) then they need not be mentioned here.
