abcd0b197eb330511c5f85ae50168bc1bc946f6d
David Asomatarihsh <dkarapetyan@daveiscool.(none)>
2013-04-27 19:17:08 -0700

    the jar was broken because of relative_require in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 if ARGV[0] == 'agent_node'
-  require_relative '../lib/agent_server/agent'
+  require 'agent_server/agent'
 elsif ARGV[0] == 'registration_node'
-  require_relative '../lib/registration_server/registration_server'
+  require 'registration_server/registration_server'
 else
   puts "Please specify the type of node to start: agent_node | registration_node."
   puts "e.g. java -jar rpc.jar [agent_node|registration_node] [options]"
